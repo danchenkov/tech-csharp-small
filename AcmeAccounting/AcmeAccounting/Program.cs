c504ea40472957bec360052e4d49d9a20647ae03
danchenkov <alexei.d@smpstaff.org>
2019-03-26 10:56:46 -0700

    C# Course: Assignment #33

---
+++ AcmeAccounting/AcmeAccounting/Program.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+class Program {
+    static void Main(string[] args) {
+        Console.WriteLine("Welcome to Acme Accounting Systems");
+        Console.WriteLine("Remember, we’re “accounting” on you!");
+        Console.ReadLine();
+    }
+}
