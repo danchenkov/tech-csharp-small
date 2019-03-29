40e49b9089c5433372e714403a9cb6ab433615a7
danchenkov <alexei.d@smpstaff.org>
2019-03-29 11:14:16 -0700

    Insurance Qualification program

---
+++ InsuranceQualification/InsuranceQualification/InsuranceQualification.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Car Insurance");
+
+        int age;
+        do
+        {
+            Console.WriteLine("What is your age?");
+        } while (!int.TryParse(Console.ReadLine(), out age));
+
+        Console.WriteLine("Have you ever had a DUI?");
+        bool hadDUI = true;
+        switch (Console.ReadLine()) {
+            case "NO":
+            case "No":
+            case "no":
+            case "Nope":
+            case "nope":
+            case "N":
+            case "n":
+                hadDUI = false;
+                break;
+        }
+
+        int speedingTickets;
+        do
+        {
+            Console.WriteLine("How many speeding tickets to you have?");
+        } while (!int.TryParse(Console.ReadLine(), out speedingTickets));
+
+        Console.WriteLine("Qualified?");
+        Console.WriteLine(age > 15 && !hadDUI && speedingTickets <= 3);
+
+        Console.Read();
+    }
+}
