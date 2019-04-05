d85d2e6acea5816435943e61760b5fd66c2dbe03
danchenkov <alexei.d@smpstaff.org>
2019-04-05 11:36:27 -0700

    Override with output parameters

---
+++ DividingTest/DividingTest/Divider.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DividingTest
+{
+    class Divider
+    {
+        public int half(int a)
+        {
+            return a / 2;
+        }
+
+        public void half(int a, out int b)
+        {
+            b = a / 2;
+        }
+    }
+}
---
+++ DividingTest/DividingTest/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DividingTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Divider divide = new Divider();
+
+            Int32 halfme;
+            do
+            {
+                Console.WriteLine("Number?");
+            } while (!Int32.TryParse(Console.ReadLine(), out halfme));
+
+            Console.WriteLine(divide.half(halfme));
+
+            Console.WriteLine("______________________________");
+
+            Int32 halfmetoo;
+            do
+            {
+                Console.WriteLine("Number?");
+            } while (!Int32.TryParse(Console.ReadLine(), out halfmetoo));
+
+            divide.half(halfmetoo, out int result);
+            Console.WriteLine(result);
+
+            Console.ReadLine();
+        }
+    }
+}
