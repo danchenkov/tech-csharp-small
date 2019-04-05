c1e9e3523cf104dcf5bb8741f64bde21442b10ee
danchenkov <alexei.d@smpstaff.org>
2019-04-05 11:12:21 -0700

    Math Operations with variadic parameters

---
+++ VariadicParameters/VariadicParameters/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Variadic v = new Variadic();
+
+        int numberOne;
+        do
+        {
+            Console.WriteLine("Number One?");
+        } while (!Int32.TryParse(Console.ReadLine(), out numberOne));
+
+        int numberTwo;
+        Console.WriteLine("Number Two (optional)?");
+        Int32.TryParse(Console.ReadLine(), out numberTwo);
+
+        Console.WriteLine(v.MathOperation(numberOne, numberTwo));
+
+        Console.WriteLine("_____________________________");
+        do
+        {
+            Console.WriteLine("Number One?");
+        } while (!Int32.TryParse(Console.ReadLine(), out numberOne));
+
+        Console.WriteLine("Number Two (optional)?");
+        Int32.TryParse(Console.ReadLine(), out numberTwo);
+
+        Console.WriteLine(v.MathOperation(numberOne, numberTwo));
+
+        Console.WriteLine("_____________________________");
+        do
+        {
+            Console.WriteLine("Number One?");
+        } while (!Int32.TryParse(Console.ReadLine(), out numberOne));
+
+        Console.WriteLine("Number Two (optional, will ignore)?");
+        Int32.TryParse(Console.ReadLine(), out numberTwo);
+
+        Console.WriteLine(v.MathOperation(numberOne));
+
+        Console.ReadLine();
+    }
+}
---
+++ VariadicParameters/VariadicParameters/Variadic.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+class Variadic
+{
+    public int MathOperation(int a, int b = 0)
+    {
+        return a + b;
+    }
+}
