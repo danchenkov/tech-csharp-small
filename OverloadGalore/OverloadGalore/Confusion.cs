f07c59c6763ed6d900aa8d72ece7e864b2c17bc5
danchenkov <alexei.d@smpstaff.org>
2019-04-05 10:59:04 -0700

    Fiddling with overloaded functions

---
+++ OverloadGalore/OverloadGalore/Confusion.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+class Confusion
+{
+    public int GuessWhat(int input)
+    {
+        return input * 2;
+    }
+
+    public int GuessWhat(decimal input)
+    {
+        return (int)input * 10;
+    }
+
+    public int GuessWhat(string input)
+    {
+        Int32.TryParse(input, out int intermediate);
+        return intermediate * 5;
+    }
+}
---
+++ OverloadGalore/OverloadGalore/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Confusion confusion = new Confusion();
+        int result = confusion.GuessWhat(12);
+        Console.WriteLine(result);
+
+        result = confusion.GuessWhat((decimal)12.2);
+        Console.WriteLine(result);
+
+        result = confusion.GuessWhat("13");
+        Console.WriteLine(result);
+
+        Console.ReadLine();
+    }
+}
