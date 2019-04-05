5326e3f193744cbaf920ddb91f314c5c8dd88429
danchenkov <alexei.d@smpstaff.org>
2019-04-05 10:38:38 -0700

    call class methods

---
+++ IntegerParameters/IntegerParameters/Handle.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+class Handle
+{
+    public int total;
+
+    public void Process(int a, int b)
+    {
+        total = a * 2;
+        Console.WriteLine(b);
+    }
+}
---
+++ IntegerParameters/IntegerParameters/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Handle handle = new Handle();
+        handle.Process(1, 2);
+        Console.WriteLine(handle.total);
+
+        handle.Process(a: 2, b: 7);
+        Console.WriteLine(handle.total);
+
+        Console.ReadLine();
+    }
+}
