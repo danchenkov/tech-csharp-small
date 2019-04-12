162a6108a30aefc33f6594d5e3a593215c840bf4
danchenkov <alexei.d@smpstaff.org>
2019-04-12 10:51:25 -0700

    Days of week

---
+++ DaysOfWeek/DaysOfWeek/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+class Program
+{
+    enum DaysOfWeek
+    {
+        Monday,
+        Tuesday,
+        Wednesday,
+        Thursday,
+        Friday,
+        Saturday,
+        Sunday
+    }
+    static void Main(string[] args)
+    {
+        DayOfWeek dow;
+        do
+        {
+            Console.WriteLine("Day of Week?");
+        } while (!DayOfWeek.TryParse(Console.ReadLine(), out dow));
+
+        Console.WriteLine("You are right, " + dow + " is a day of week");
+        Console.ReadLine();
+    }
+}
