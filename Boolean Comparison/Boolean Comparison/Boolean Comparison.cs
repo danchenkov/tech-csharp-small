1951a1206c78ba0de757e25d161de3c96d2a3745
danchenkov <alexei.d@smpstaff.org>
2019-04-12 10:00:35 -0700

    Keep the while in the Program class.

--- Boolean Comparison/Boolean Comparison/Boolean Comparison.cs
+++ Boolean Comparison/Boolean Comparison/Boolean Comparison.cs
@@ -1,6 +1,6 @@
 ﻿using System;
 
-class Program
+class Comparison
 {
     static void Main(string[] args)
     {
--- Boolean Comparison/Boolean Comparison/Program.cs
+++ Boolean Comparison/Boolean Comparison/Program.cs
@@ -1,15 +1,25 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace Boolean_Comparison
+class Program
 {
-    class Program
+    static void Main(string[] args)
     {
-        static void Main(string[] args)
+        Console.WriteLine("Bring a brick and measure its weight");
+
+        int totalWeight = 0;
+        int weight;
+        while (totalWeight < 100)
         {
+            do
+            {
+                Console.WriteLine("What is the brick's weight?");
+            } while (!int.TryParse(Console.ReadLine(), out weight));
+            totalWeight += weight;
+
+            Console.WriteLine("Not there yet (need 100). Currently at " + totalWeight + " tons");
         }
+
+        Console.WriteLine("Yay! You created a pile of bricks with overall weight of " + totalWeight + " tons");
+        Console.Read();
     }
 }
