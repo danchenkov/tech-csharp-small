447bc7abaf8562b3aad87c75f28d15acb6b5ca54
danchenkov <alexei.d@smpstaff.org>
2019-04-13 10:08:16 -0700

    create a struct with decimal type

---
+++ DataTypes/DataTypes/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+class Program
+{
+    struct Number
+    {
+        public decimal Amount;
+    }
+    static void Main(string[] args)
+    {
+        Number number = new Number();
+        number.Amount = 3.14159265358979323M;
+
+        Console.WriteLine(number.Amount);
+        Console.ReadLine();
+    }
+}
