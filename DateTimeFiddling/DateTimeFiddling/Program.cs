3e7883bf140f29ea11507541d82828926a502435
Alexei Danchenkov <alexei.d@smpstaff.org>
2019-04-23 11:16:29 -0700

    Hours from now

---
+++ DateTimeFiddling/DateTimeFiddling/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace DateTimeFiddling
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(DateTime.Now);
+
+            int delay;
+            do
+            {
+                Console.WriteLine("Hours from now?");
+            } while (!int.TryParse(Console.ReadLine(), out delay));
+
+            Console.WriteLine("In {0} hours it will be {1}", delay, DateTime.Now.AddHours(delay));
+        }
+    }
+}
