9f87d1425036a99ae3cbbc8fefeca5c5afd8aeb0
Alexei Danchenkov <alexei.d@smpstaff.org>
2019-04-23 10:12:37 -0700

    Logging test

---
+++ Logging/Logging/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.IO;
+
+namespace Logging
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int age;
+            do
+            {
+                Console.WriteLine("What is your age?");
+            } while (!int.TryParse(Console.ReadLine(), out age));
+
+            File.WriteAllText("C:\\Users\\IEUser\\Code\\logs\\test-log.txt", "User with age " + age);
+            Console.Write("Your age " + age + " has been logged to a file");
+        }
+    }
+}
