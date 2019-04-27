a1db910253bbe611f4301e0cec92878c13f33a14
Alexei Danchenkov <alexei.d@smpstaff.org>
2019-04-27 11:01:47 -0700

    Year guessing

---
+++ Exceptional/Exceptional/NegativeAgeException.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Exceptional
+{
+    public class NegativeAgeException : Exception
+    {
+        public NegativeAgeException()
+            : base() { }
+        public NegativeAgeException(string message)
+            : base(message) { }
+    }
+}
---
+++ Exceptional/Exceptional/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace Exceptional
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime now = DateTime.Now;
+            int guessYear;
+            bool guessAgain = true;
+            
+            while(guessAgain)
+            {
+                Console.WriteLine("What is your age? (in years, use digits only)");
+                string ageString = Console.ReadLine();
+                guessAgain = false;
+                try
+                {
+                    int age = Int32.Parse(ageString);
+                    if (age <= 0) { throw new NegativeAgeException(); }
+                    guessYear = now.AddYears(-age).Year;
+
+                    Console.WriteLine("You must have been born in {0} or {1}", guessYear - 1, guessYear);
+                    Console.ReadLine();
+                }
+                catch (FormatException)
+                {
+                    Console.WriteLine("Bad Format: {0}", ageString);
+                    guessAgain = true;
+                }
+                catch (OverflowException)
+                {
+                    Console.WriteLine("Overflow Error: {0}", ageString);
+                    guessAgain = true;
+                }
+                catch (NegativeAgeException)
+                {
+                    Console.WriteLine("It is unlikely that you have that age: {0}", ageString);
+                    guessAgain = true;
+                }
+            }
+
+
+        }
+    }
+}
