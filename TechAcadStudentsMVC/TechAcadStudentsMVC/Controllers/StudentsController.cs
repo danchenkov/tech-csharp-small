e9acc661ee26981b5e464e8509c53c243df92c43
Alexei Danchenkov <alexei.d@smpstaff.org>
2019-05-04 11:05:36 -0700

    Newsletter

---
+++ TechAcadStudentsMVC/TechAcadStudentsMVC/Controllers/StudentsController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace TechAcadStudentsMVC.Controllers
+{
+    public class StudentsController : Controller
+    {
+        private string _connectionString = @"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=master;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
+
+        // GET: Students
+        public ActionResult Index()
+        {
+            string queryString = "SELECT * FROM Students";
+            List<Student> students = new List<Student>();
+
+            using (SqlConnection connection = new SqlConnection(_connectionString))
+            {
+                SqlCommand command = new SqlCommand(queryString, connection);
+                connection.Open();
+                SqlDataReader reader = command.ExecuteReader();
+
+                while (reader.Read())
+                {
+                    StudentsController student = new StudentsController();
+                    student.Id = Convert.ToInt32(reader["Id"]);
+                    student.FirstName = reader["FirstName"].ToString();
+                    student.LastName = reader["LastName"].ToString();
+                    students.Add(student);
+                }
+                connection.Close();
+            }
+            return View(students);
+        }
+    }
+}
