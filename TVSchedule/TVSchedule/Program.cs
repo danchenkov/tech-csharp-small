b152ddd121f84f3496b593aa4e66527b3a9a4699
Alexei Danchenkov <alexei.d@smpstaff.org>
2019-05-09 11:05:33 -0700

    MVC for Knockout

--- TVSchedule/TVSchedule/Program.cs
+++ TVSchedule/TVSchedule/Program.cs
@@ -8,7 +8,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
 
-namespace TV_Schedule
+namespace TVSchedule
 {
     public class Program
     {
